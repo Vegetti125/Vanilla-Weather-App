dade116a5528fd0abb902985fe7e6e1368ca6d27
Vegetti125 <130096481+Vegetti125@users.noreply.github.com>
2023-04-22 18:35:33 -0500

    added displayForecast Function
    array created for days of the week. function set to call each day and col

--- src/app.js
+++ src/app.js
@@ -20,6 +20,39 @@ function formatDate(timestamp) {
     let day = days[date.getDay()];
     return `${day} ${hours}:${minutes}`;
 }
+function displayForecast(){
+    let forecastElement = document.querySelector("#forecast");
+
+    let forecastHTML= `<div class="row">`;
+    let days = ["Sun", "Mon", "Tue", "Wed","Thu", "Fri"];
+    days.forEach(function(day) {
+    forecastHTML= 
+forecastHTML + 
+`
+        <div class="col-2">
+            <div class="weather-forecast-date">${day}</div>
+            <img
+            src="http://openweathermap.org/img/wn/50d@2x.png"
+            alt=""
+            width="42"
+            /> 
+            <div class="weather-forecast-temperatures">
+            
+            <span class="weather-forecast-temperature-max">   
+                 18° 
+                 </span>
+                 <span class="weather-forecast-temperature-min">
+                    12°
+                 </span>
+            </div>
+        </div>
+    `;
+});
+
+
+    forecastHTML= forecastHTML + `</div>`;
+    forecastElement.innerHTML= forecastHTML;
+}
 
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
@@ -77,6 +110,7 @@ function displayFahrenheitTemperature(event) {
 }
 
 search("Chicago");
+displayForecast();
 
 let fahrenheitTemperature = null;
 
